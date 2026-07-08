9dda30f4135380f8f9195a366bc22da76a05c8ed
aerciompr <aerciompr@gmail.com>
2026-07-08 17:31:41 -0300

    Inclui config.php generico para leitura de variaveis de ambiente no Docker

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASS', getenv('DB_PASS') !== false ? getenv('DB_PASS') : '');
+define('DB_NAME', getenv('DB_NAME') ?: 'justica_senhas');
+define('SYS_NAME', getenv('SYS_NAME') ?: 'Justiça Federal');
+
